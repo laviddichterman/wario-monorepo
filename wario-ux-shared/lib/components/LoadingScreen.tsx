52863a6b6568537111dad4f8bb32151464e79e2a
Dave Lichterman <me@lavid.me>
2025-10-12 22:03:49 -0700

    restart

--- wario-ux-shared/lib/components/LoadingScreen.tsx
+++
@@ -1,5 +0,0 @@
-import { Logo } from './Logo';
-
-import { FullScreenPulsingContainer } from '../containers/FullScreenPulsingContainer';
-
-export const LoadingScreen = () => <FullScreenPulsingContainer children={<Logo sx={{ width: 64, height: 64 }} />} />
