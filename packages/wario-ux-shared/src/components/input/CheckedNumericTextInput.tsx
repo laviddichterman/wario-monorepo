6eb2925989879f53ddf0b3f67be57745bb554217
Dave Lichterman <me@lavid.me>
2025-10-28 18:58:39 -0700

    ditry check on CheckedNumericInput, also make it work properly?

--- packages/wario-ux-shared/src/components/input/CheckedNumericTextInput.tsx
+++ packages/wario-ux-shared/src/components/input/CheckedNumericTextInput.tsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 
 import { TextField, type TextFieldProps } from '@mui/material';
 
-import { type InputNumberValue, type NumberTransformPropsAllowEmpty, type NumberTransformPropsNoEmpty, transformValueOnBlur, transformValueOnChange } from '@wcp/wario-shared';
+import { type DistributiveOmit, type InputNumberValue, type NumberTransformPropsAllowEmpty, type NumberTransformPropsNoEmpty, transformValueOnBlur, transformValueOnChange } from '@wcp/wario-shared';
 
-export type CheckedNumericInputProps = Omit<TextFieldProps, 'value' | 'onChange' | 'onBlur'> & {
+export type CheckedNumericInputProps = DistributiveOmit<TextFieldProps, 'value' | 'onChange' | 'onBlur'> & {
   value: InputNumberValue;
   step?: number;
   inputMode: 'numeric' | 'decimal' | 'text';
@@ -17,29 +17,35 @@ export type CheckedNumericInputProps = Omit<TextFieldProps, 'value' | 'onChange'
 
 export function CheckedNumericInput({ onChange, step, pattern, inputMode, value, numberProps, ...other }: CheckedNumericInputProps) {
   const [inputText, setInputText] = useState(numberProps.formatFunction(value));
+  const [dirty, setDirty] = useState(false);
   // Keep local input text in sync with value prop changes
   useEffect(() => {
-    setInputText(numberProps.formatFunction(value));
-  }, [numberProps, value]);
+    if (!dirty) {
+      setInputText(numberProps.formatFunction(value));
+    }
+  }, [value, numberProps, dirty]);
+  // console.log(`CheckedNumericInput render: value=${JSON.stringify(value)} inputText=${JSON.stringify(inputText)}`);
   return (
     <TextField
       {...other}
       value={inputText}
       onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
         const result = transformValueOnChange(numberProps, e.target.value);
+        setDirty(true);
         setInputText(result.inputText);
-        if (result.value !== "") {
-          onChange(result.value);
-        }
+        // if (result.value !== "") {
+        //   onChange(result.value);
+        // }
       }}
       onBlur={(e: React.FocusEvent<HTMLInputElement>) => {
         const new_val = transformValueOnBlur(numberProps, e.target.value);
         setInputText(new_val.inputText);
         // @ts-expect-error -- we know this is fine based on the props type since the onChange function will match with allowEmpty
         onChange(new_val.value);
+        setDirty(false);
       }}
       // eslint-disable-next-line @typescript-eslint/no-misused-spread
-      slotProps={{ ...other.slotProps, htmlInput: { step: step, pattern: pattern, inputMode: inputMode, min: numberProps.min, max: numberProps.max, ...(other.slotProps?.htmlInput ?? {}) } }}
+      slotProps={{ ...other.slotProps, input: { inputMode }, htmlInput: { step: step, pattern: pattern, inputMode: inputMode, min: numberProps.min, max: numberProps.max, ...(other.slotProps?.htmlInput ?? {}) } }}
     />
   )
 }
