0f788a19b8adbaac8782153da14321cae5fc0952
Dave Lichterman <me@lavid.me>
2025-11-02 17:37:22 -0800

    same @ aliasing for wario-pos

--- apps/wario-pos/vite.config.ts
+++ apps/wario-pos/vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import { fileURLToPath } from 'node:url';
 
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
@@ -23,20 +23,14 @@ export default defineConfig({
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
-  build: { target: 'es2022' },
-  optimizeDeps: { esbuildOptions: { target: 'es2022' } },
   server: { port: PORT, host: true },
   preview: { port: PORT, host: true },
+  build: { target: 'es2022' },
+  optimizeDeps: { esbuildOptions: { target: 'es2022' } },
   resolve: {
-    alias: [
-      {
-        find: /^~(.+)/,
-        replacement: path.resolve(process.cwd(), 'node_modules/$1'),
-      },
-      {
-        find: /^src(.+)/,
-        replacement: path.resolve(process.cwd(), 'src/$1'),
-      },
-    ],
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'src': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
 });
