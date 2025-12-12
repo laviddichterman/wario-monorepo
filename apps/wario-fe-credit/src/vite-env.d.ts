87a49eac923d2804fc7b31f02cf8620ce0de30d0
Dave Lichterman <me@lavid.me>
2025-12-11 22:50:28 -0800

    maybe someday i'll put the square app id in the env. not today though

--- apps/wario-fe-credit/src/vite-env.d.ts
+++ apps/wario-fe-credit/src/vite-env.d.ts
@@ -4,6 +4,8 @@ declare const __APP_VERSION__: string;
 interface ImportMetaEnv {
   readonly VITE_HOST_API_KEY: string;
   readonly VITE_SOCKETIO_NS: string;
+  // readonly SQUARE_APPLICATION_ID: string;
+  // readonly SQUARE_LOCATION_ID: string;
 }
 
 interface ImportMeta {
